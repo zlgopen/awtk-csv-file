2b8252118852e9fafecd3e031cde836e696db3f7
lixianjing <xianjimli@hotmail.com>
2023-12-07 09:14:00 +0800

    support query

--- tests/csv_file_object_test.cc
+++ tests/csv_file_object_test.cc
@@ -2,10 +2,11 @@
 #include "tkc/fs.h"
 #include "tkc/path.h"
 #include "tkc/mem.h"
+#include "tkc/utils.h"
 #include "csv_file.h"
 #include "csv_file_object.h"
 
-TEST(csv_file_object, basic) {
+TEST(csv_file_object, basic1) {
   const char* str = "aa,bb,cc\n11,12,13\n21,22,23";
   csv_file_t* csv = csv_file_create_with_buff(str, strlen(str), ',');
   object_t* obj = csv_file_object_create(csv);
@@ -14,39 +15,78 @@ TEST(csv_file_object, basic) {
   ASSERT_STREQ(object_get_prop_str(obj, "[0].[0]"), "aa");
   ASSERT_STREQ(object_get_prop_str(obj, "[0].[1]"), "bb");
   ASSERT_STREQ(object_get_prop_str(obj, "[0].[2]"), "cc");
-  
+
   ASSERT_STREQ(object_get_prop_str(obj, "[1].[0]"), "11");
   ASSERT_STREQ(object_get_prop_str(obj, "[1].[1]"), "12");
   ASSERT_STREQ(object_get_prop_str(obj, "[1].[2]"), "13");
-  
+
   ASSERT_EQ(object_set_prop_int(obj, "[1].[0]", 111), RET_OK);
   ASSERT_EQ(object_set_prop_int(obj, "[1].[1]", 122), RET_OK);
   ASSERT_EQ(object_set_prop_int(obj, "[1].[2]", 133), RET_OK);
-  
+
   ASSERT_STREQ(object_get_prop_str(obj, "[1].[0]"), "111");
   ASSERT_STREQ(object_get_prop_str(obj, "[1].[1]"), "122");
   ASSERT_STREQ(object_get_prop_str(obj, "[1].[2]"), "133");
-  
+
   ASSERT_EQ(object_remove_prop(obj, "[1]"), RET_OK);
   ASSERT_EQ(object_get_prop_int(obj, "#size", 0), 2);
-  
+
   ASSERT_EQ(object_exec(obj, OBJECT_CMD_CLEAR, NULL), RET_OK);
   ASSERT_EQ(object_get_prop_int(obj, "#size", 0), 0);
-  
+
   ASSERT_EQ(object_exec(obj, OBJECT_CMD_ADD, "11,12,13"), RET_OK);
   ASSERT_EQ(object_get_prop_int(obj, "#size", 0), 1);
-  
+
   ASSERT_EQ(object_exec(obj, OBJECT_CMD_ADD, "21,22,23"), RET_OK);
   ASSERT_EQ(object_get_prop_int(obj, "#size", 0), 2);
-  
+
   ASSERT_EQ(object_exec(obj, OBJECT_CMD_REMOVE, "0"), RET_OK);
   ASSERT_EQ(object_get_prop_int(obj, "#size", 0), 1);
 
   OBJECT_UNREF(obj);
 }
 
+TEST(csv_file_object, basic2) {
+  const char* str = "aa,bb,cc\n11,12,13\n21,22,23";
+  csv_file_t* csv = csv_file_create_with_buff(str, strlen(str), ',');
+  object_t* obj = csv_file_object_create(csv);
 
-TEST(csv_file_object, title ) {
+  ASSERT_EQ(object_get_prop_int(obj, "#size", 0), 3);
+  ASSERT_STREQ(object_get_prop_str(obj, "0.[0]"), "aa");
+  ASSERT_STREQ(object_get_prop_str(obj, "0.[1]"), "bb");
+  ASSERT_STREQ(object_get_prop_str(obj, "0.[2]"), "cc");
+
+  ASSERT_STREQ(object_get_prop_str(obj, "1.[0]"), "11");
+  ASSERT_STREQ(object_get_prop_str(obj, "1.[1]"), "12");
+  ASSERT_STREQ(object_get_prop_str(obj, "1.[2]"), "13");
+
+  ASSERT_EQ(object_set_prop_int(obj, "1.[0]", 111), RET_OK);
+  ASSERT_EQ(object_set_prop_int(obj, "1.[1]", 122), RET_OK);
+  ASSERT_EQ(object_set_prop_int(obj, "1.[2]", 133), RET_OK);
+
+  ASSERT_STREQ(object_get_prop_str(obj, "1.0"), "111");
+  ASSERT_STREQ(object_get_prop_str(obj, "1.1"), "122");
+  ASSERT_STREQ(object_get_prop_str(obj, "1.2"), "133");
+
+  ASSERT_EQ(object_remove_prop(obj, "1"), RET_OK);
+  ASSERT_EQ(object_get_prop_int(obj, "#size", 0), 2);
+
+  ASSERT_EQ(object_exec(obj, OBJECT_CMD_CLEAR, NULL), RET_OK);
+  ASSERT_EQ(object_get_prop_int(obj, "#size", 0), 0);
+
+  ASSERT_EQ(object_exec(obj, OBJECT_CMD_ADD, "11,12,13"), RET_OK);
+  ASSERT_EQ(object_get_prop_int(obj, "#size", 0), 1);
+
+  ASSERT_EQ(object_exec(obj, OBJECT_CMD_ADD, "21,22,23"), RET_OK);
+  ASSERT_EQ(object_get_prop_int(obj, "#size", 0), 2);
+
+  ASSERT_EQ(object_exec(obj, OBJECT_CMD_REMOVE, "0"), RET_OK);
+  ASSERT_EQ(object_get_prop_int(obj, "#size", 0), 1);
+
+  OBJECT_UNREF(obj);
+}
+
+TEST(csv_file_object, title) {
   const char* str = "aa,bb,cc\n11,12,13\n21,22,23";
   csv_file_t* csv = csv_file_create_with_buff(str, strlen(str), ',');
   object_t* obj = csv_file_object_create(csv);
@@ -55,42 +95,42 @@ TEST(csv_file_object, title ) {
   ASSERT_STREQ(object_get_prop_str(obj, "[0].aa"), "aa");
   ASSERT_STREQ(object_get_prop_str(obj, "[0].bb"), "bb");
   ASSERT_STREQ(object_get_prop_str(obj, "[0].cc"), "cc");
-  
+
   ASSERT_STREQ(object_get_prop_str(obj, "[1].aa"), "11");
   ASSERT_STREQ(object_get_prop_str(obj, "[1].bb"), "12");
   ASSERT_STREQ(object_get_prop_str(obj, "[1].cc"), "13");
-  
+
   ASSERT_EQ(object_set_prop_int(obj, "[1].aa", 111), RET_OK);
   ASSERT_EQ(object_set_prop_int(obj, "[1].bb", 122), RET_OK);
   ASSERT_EQ(object_set_prop_int(obj, "[1].cc", 133), RET_OK);
-  
+
   ASSERT_STREQ(object_get_prop_str(obj, "[1].aa"), "111");
   ASSERT_STREQ(object_get_prop_str(obj, "[1].bb"), "122");
   ASSERT_STREQ(object_get_prop_str(obj, "[1].cc"), "133");
-  
+
   ASSERT_EQ(object_remove_prop(obj, "[1]"), RET_OK);
   ASSERT_EQ(object_get_prop_int(obj, "#size", 0), 2);
-  
+
   ASSERT_EQ(object_exec(obj, OBJECT_CMD_CLEAR, NULL), RET_OK);
   ASSERT_EQ(object_get_prop_int(obj, "#size", 0), 0);
-  
+
   ASSERT_EQ(object_exec(obj, OBJECT_CMD_ADD, "11,12,13"), RET_OK);
   ASSERT_EQ(object_get_prop_int(obj, "#size", 0), 1);
-  
+
   ASSERT_EQ(object_exec(obj, OBJECT_CMD_ADD, "21,22,23"), RET_OK);
   ASSERT_EQ(object_get_prop_int(obj, "#size", 0), 2);
-  
+
   ASSERT_EQ(object_exec(obj, OBJECT_CMD_REMOVE, "[0]"), RET_OK);
   ASSERT_EQ(object_get_prop_int(obj, "#size", 0), 1);
-  
+
   ASSERT_EQ(object_exec(obj, OBJECT_CMD_ADD, "31,32,33"), RET_OK);
   ASSERT_EQ(object_get_prop_int(obj, "#size", 0), 2);
-  
+
   ASSERT_EQ(object_exec(obj, OBJECT_CMD_SAVE, "test1.csv"), RET_OK);
-  
+
   ASSERT_EQ(object_exec(obj, OBJECT_CMD_REMOVE, "0"), RET_OK);
   ASSERT_EQ(object_get_prop_int(obj, "#size", 0), 1);
-  
+
   ASSERT_EQ(object_exec(obj, OBJECT_CMD_RELOAD, NULL), RET_OK);
   ASSERT_EQ(object_get_prop_int(obj, "#size", 0), 1);
 
@@ -111,18 +151,18 @@ TEST(csv_file_object, checked) {
 
   ASSERT_EQ(object_set_prop_bool(obj, "[4].checked", TRUE), RET_OK);
   ASSERT_EQ(object_get_prop_bool(obj, "[4].checked", FALSE), TRUE);
-  
+
   ASSERT_EQ(object_exec(obj, OBJECT_CMD_REMOVE_CHECKED, NULL), RET_OK);
   ASSERT_EQ(object_get_prop_int(obj, "#size", 0), 2);
-  
+
   ASSERT_STREQ(object_get_prop_str(obj, "[0].0"), "11");
   ASSERT_STREQ(object_get_prop_str(obj, "[0].1"), "12");
   ASSERT_STREQ(object_get_prop_str(obj, "[0].2"), "13");
-  
+
   ASSERT_STREQ(object_get_prop_str(obj, "[1].0"), "31");
   ASSERT_STREQ(object_get_prop_str(obj, "[1].1"), "32");
   ASSERT_STREQ(object_get_prop_str(obj, "[1].2"), "33");
-  
+
   ASSERT_EQ(object_exec(obj, OBJECT_CMD_REMOVE_CHECKED, NULL), RET_OK);
   ASSERT_EQ(object_get_prop_int(obj, "#size", 0), 2);
 
@@ -130,7 +170,7 @@ TEST(csv_file_object, checked) {
   ASSERT_EQ(object_get_prop_bool(obj, "[0].checked", FALSE), TRUE);
   ASSERT_EQ(object_set_prop_bool(obj, "[1].checked", TRUE), RET_OK);
   ASSERT_EQ(object_get_prop_bool(obj, "[1].checked", FALSE), TRUE);
-  
+
   ASSERT_EQ(object_exec(obj, OBJECT_CMD_REMOVE_CHECKED, NULL), RET_OK);
   ASSERT_EQ(object_get_prop_int(obj, "#size", 0), 0);
   ASSERT_EQ(csv_file_object_get_csv(obj), csv);
@@ -144,11 +184,11 @@ TEST(csv_file_object, load_from_buff) {
   object_t* obj = csv_file_object_load_from_buff(str, strlen(str), ',');
 
   wbuffer_init_extendable(&wb);
-  
+
   ASSERT_STREQ(object_get_prop_str(obj, "[0].aa"), "aa");
   ASSERT_STREQ(object_get_prop_str(obj, "[0].bb"), "bb");
   ASSERT_STREQ(object_get_prop_str(obj, "[0].cc"), "cc");
-  
+
   ASSERT_STREQ(object_get_prop_str(obj, "[1].aa"), "11");
   ASSERT_STREQ(object_get_prop_str(obj, "[1].bb"), "12");
   ASSERT_STREQ(object_get_prop_str(obj, "[1].cc"), "13");
@@ -163,7 +203,7 @@ TEST(csv_file_object, load_from_buff) {
 
 TEST(csv_file_object, load_from_file) {
   object_t* obj = NULL;
-  char filename[MAX_PATH+1] = {0};
+  char filename[MAX_PATH + 1] = {0};
   const char* str = "aa,bb,cc\n11,12,13\n21,22,23\n31,32,33\n41,42,43";
 
   path_prepend_temp_path(filename, "test.csv");
@@ -174,7 +214,7 @@ TEST(csv_file_object, load_from_file) {
   ASSERT_STREQ(object_get_prop_str(obj, "[0].aa"), "aa");
   ASSERT_STREQ(object_get_prop_str(obj, "[0].bb"), "bb");
   ASSERT_STREQ(object_get_prop_str(obj, "[0].cc"), "cc");
-  
+
   ASSERT_STREQ(object_get_prop_str(obj, "[1].aa"), "11");
   ASSERT_STREQ(object_get_prop_str(obj, "[1].bb"), "12");
   ASSERT_STREQ(object_get_prop_str(obj, "[1].cc"), "13");
@@ -187,7 +227,7 @@ TEST(csv_file_object, load_from_file) {
   ASSERT_STREQ(object_get_prop_str(obj, "[0].aa"), "aa");
   ASSERT_STREQ(object_get_prop_str(obj, "[0].bb"), "bb");
   ASSERT_STREQ(object_get_prop_str(obj, "[0].cc"), "cc");
-  
+
   ASSERT_STREQ(object_get_prop_str(obj, "[1].aa"), "11");
   ASSERT_STREQ(object_get_prop_str(obj, "[1].bb"), "12");
   ASSERT_STREQ(object_get_prop_str(obj, "[1].cc"), "13");
@@ -195,5 +235,52 @@ TEST(csv_file_object, load_from_file) {
   OBJECT_UNREF(obj);
 
   fs_remove_file(os_fs(), filename);
+}
 
+static ret_t csv_file_object_filter1(void* ctx, tk_object_t* args, uint32_t index, csv_row_t* row) {
+  const char* col0 = csv_row_get(row, 0);
+  int32_t value0 = tk_atoi(col0);
+  int32_t start = tk_object_get_prop_int(args, CSV_QUERY_PREFIX "0_start", 0);
+  int32_t end = tk_object_get_prop_int(args, CSV_QUERY_PREFIX "0_end", 0);
+
+  if (value0 >= start && value0 <= end) {
+    return RET_OK;
+  } else {
+    return RET_FAIL;
+  }
+}
+
+TEST(csv_file_object, filter1) {
+  const char* str = "11,12,13\n21,22,23\n31,32,33\n41,42,43";
+  csv_file_t* csv = csv_file_create_with_buff(str, strlen(str), ',');
+  object_t* obj = csv_file_object_create(csv);
+
+  ASSERT_EQ(tk_object_get_prop_int(obj, TK_OBJECT_PROP_SIZE, 0), 4);
+  ASSERT_EQ(csv_file_object_set_filter(obj, csv_file_object_filter1, NULL), RET_OK);
+  ASSERT_EQ(tk_object_set_prop_int(obj, CSV_QUERY_PREFIX "0_start", 20), RET_OK);
+  ASSERT_EQ(tk_object_set_prop_int(obj, CSV_QUERY_PREFIX "0_end", 30), RET_OK);
+
+  ASSERT_EQ(tk_object_set_prop_int(obj, CSV_QUERY_PREFIX "0_end", 40), RET_OK);
+  ASSERT_EQ(object_exec(obj, CSV_CMD_QUERY, NULL), RET_OK);
+  ASSERT_EQ(tk_object_get_prop_int(obj, TK_OBJECT_PROP_SIZE, 0), 2);
+  
+  ASSERT_EQ(tk_object_set_prop_int(obj, CSV_QUERY_PREFIX "0_end", 50), RET_OK);
+  ASSERT_EQ(object_exec(obj, CSV_CMD_QUERY, NULL), RET_OK);
+  ASSERT_EQ(tk_object_get_prop_int(obj, TK_OBJECT_PROP_SIZE, 0), 3);
+  
+  
+  ASSERT_EQ(tk_object_set_prop_int(obj, CSV_QUERY_PREFIX "0_start", 0), RET_OK);
+  ASSERT_EQ(tk_object_set_prop_int(obj, CSV_QUERY_PREFIX "0_end", 50), RET_OK);
+  ASSERT_EQ(object_exec(obj, CSV_CMD_QUERY, NULL), RET_OK);
+  ASSERT_EQ(tk_object_get_prop_int(obj, TK_OBJECT_PROP_SIZE, 0), 4);
+  
+  ASSERT_EQ(tk_object_set_prop_int(obj, CSV_QUERY_PREFIX "0_start", 40), RET_OK);
+  ASSERT_EQ(tk_object_set_prop_int(obj, CSV_QUERY_PREFIX "0_end", 50), RET_OK);
+  ASSERT_EQ(object_exec(obj, CSV_CMD_QUERY, NULL), RET_OK);
+  ASSERT_EQ(tk_object_get_prop_int(obj, TK_OBJECT_PROP_SIZE, 0), 1);
+  
+  ASSERT_EQ(object_exec(obj, CSV_CMD_QUERY, CSV_CMD_QUERY_ARG_CLEAR), RET_OK);
+  ASSERT_EQ(tk_object_get_prop_int(obj, TK_OBJECT_PROP_SIZE, 0), 4);
+
+  OBJECT_UNREF(obj);
 }
